7d79f46a2502168653909748f2e82f91103fcd28
Mathias Lorenz <lorenz.mathias@gmail.com>
2018-02-16 15:10:34 +0100

    Rewrote single entry in jacobi_openmp to use linear indexing.

--- 2D_poisson_openmp/jacobi_openmp.c
+++ 2D_poisson_openmp/jacobi_openmp.c
@@ -7,6 +7,8 @@
 #include "matrix_routines.h"
 #include "poisson.h"
 
+#define ACCESS_2D(A, r, c, rC) (A[(r)*(rC) + c])
+
 extern double MFLOP;
 
 void jacobi_openmp(int N_total, int maxit, double threshold,
@@ -21,6 +23,7 @@ void jacobi_openmp(int N_total, int maxit, double threshold,
     double norm_diff = 10.0;
 
 
+
     // Jacobi iteration
     bool use_tol = false;
     if (strcmp("on",getenv("USE_TOLERANCE")) == 0)
@@ -28,12 +31,29 @@ void jacobi_openmp(int N_total, int maxit, double threshold,
 
     for(k = 0; k < maxit ; k++){
         norm_diff = 0.0;
+        // Linear indexing
+        double * u_lin = *u;
+
         #pragma omp parallel for\
             private(i,j) reduction(+: norm_diff) schedule(runtime)
         for(i=1; i<N+1; i++){
             for(j=1; j<N+1; j++){
-                tmp[i][j] = frac*(u[i-1][j]+u[i+1][j]+
-                    u[i][j-1]+u[i][j+1]+step*f[i][j]);
+                //tmp[i][j] = frac*(u[i-1][j]+u[i+1][j]+
+                //    u[i][j-1]+u[i][j+1]+step*f[i][j]);
+
+                // With linear indexing
+                //tmp[i][j] -> tmp[i*N + j]
+                //tmp[i][j] = frac*(u_lin[(i-1)*N + j] + u_lin[(i+1)*N + j]
+                //                + u_lin[i*N + j-1] + u_lin[i*N + j]
+                //                + step*f[i][j]);
+
+                // Linear indexing and macro
+                tmp[i][j] = frac*( ACCESS_2D(u_lin, i-1, j, N)
+                                 + ACCESS_2D(u_lin, i+1, j, N)
+                                 + ACCESS_2D(u_lin, i, j-1, N)
+                                 + ACCESS_2D(u_lin, i, j+1, N)
+                                 + step*f[i][j]);
+
                 norm_diff += (u[i][j]-tmp[i][j])*(u[i][j]-tmp[i][j]);
             }
         }
@@ -43,7 +63,7 @@ void jacobi_openmp(int N_total, int maxit, double threshold,
         if (use_tol && (norm_diff < threshold))
             break;
     }
-    
+
     if (strcmp("matrix",getenv("OUTPUT_INFO")) == 0){
         // Print exit cause
         if(norm_diff < threshold && use_tol)
