c14ab33b667c31c27c3d1f3755f7bd9e7b21074f
Mathias Lorenz <lorenz.mathias@gmail.com>
2018-03-14 15:20:13 +0100

    Added script login_interactive.sh to login to an interactive node on hpcint.

---
+++ login_interactive.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Read in number of cores option
+if [ -z "$1" ] ; then
+	NUMCORES=4
+else
+	NUMCORES="$1"
+fi
+
+# Start interactive node
+echo "Executing the command:"
+echo "bsub -W 4:00 -q hpcint -app qrsh -env \"TERM\" -Is -n $NUMCORES \"reset; $SHELL -l\""
+echo " "
+
+bsub -W 4:00 -q hpcint -app qrsh -env "TERM" -Is -n $NUMCORES "reset; $SHELL -l"
+
+exit 0
